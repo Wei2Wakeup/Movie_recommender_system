b50b92c569dff4c27ca85326b607b2c39fba44e0
Chirs <wanwei@umich.edu>
2017-10-22 23:16:14 -0700

    Add files via upload

---
+++ src/CoOccurrenceMatrixGenerator.java
@@ -0,0 +1,80 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import java.io.IOException;
+
+public class CoOccurrenceMatrixGenerator {
+	public static class MatrixGeneratorMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+		// map method
+		@Override
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+			//value = userid \t movie1: rating, movie2: rating...
+			//key = movie1: movie2 value = 1
+			//calculate each user rating list: <movieA, movieB>
+			String line = value.toString().trim();
+			String[] items = line.split("\t");
+			String userid = items[0];
+			String[] movie_ratings =  items[1].split(",");
+
+			for(String mr1 : movie_ratings){
+				String mv1 = mr1.split(":")[0];
+				
+				for(String mr2 : movie_ratings){
+					String mv2 = mr2.split(":")[0];
+				    context.write(new Text(mv1+":"+mv2), new IntWritable(1));
+				}
+
+			}
+
+			
+		}
+	}
+
+	public static class MatrixGeneratorReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+		// reduce method
+		@Override
+		public void reduce(Text key, Iterable<IntWritable> values, Context context)
+				throws IOException, InterruptedException {
+			//key movie1:movie2 value = iterable<1, 1, 1>
+			//calculate each two movies have been watched by how many people
+
+			int sum=0;
+			for(IntWritable value : values){
+				sum+=value.get();
+			}
+			context.write(key, new IntWritable(sum));
+
+		}
+	}
+	
+	public static void main(String[] args) throws Exception{
+		
+		Configuration conf = new Configuration();
+		
+		Job job = Job.getInstance(conf);
+		job.setMapperClass(MatrixGeneratorMapper.class);
+		job.setReducerClass(MatrixGeneratorReducer.class);
+		
+		job.setJarByClass(CoOccurrenceMatrixGenerator.class);
+		
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		
+		TextInputFormat.setInputPaths(job, new Path(args[0]));
+		TextOutputFormat.setOutputPath(job, new Path(args[1]));
+		
+		job.waitForCompletion(true);
+		
+	}
+}
---
+++ src/Multiplication.java
@@ -0,0 +1,111 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.chain.ChainMapper;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Multiplication {
+	public static class CooccurrenceMapper extends Mapper<LongWritable, Text, Text, Text> {
+
+		// map method
+		@Override
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+			//input: movieB \t movieA=relation
+
+			//pass data to reducer
+			String line = value.toString().trim();
+			String[] items = line.split("\t");
+			context.write(new Text(items[0]), new Text(items[1]));
+		}
+	}
+
+	public static class RatingMapper extends Mapper<LongWritable, Text, Text, Text> {
+
+		// map method
+		@Override
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+			//input: user,movie,rating
+			//pass data to reducer
+			String line = value.toString().trim();
+			String[] items = line.split(",");
+			context.write(new Text(items[1]), new Text(items[0]+":"+items[2]));
+		}
+	}
+
+	public static class MultiplicationReducer extends Reducer<Text, Text, Text, DoubleWritable> {
+		// reduce method
+		@Override
+		public void reduce(Text key, Iterable<Text> values, Context context)
+				throws IOException, InterruptedException {
+
+			//key = movieB value = <movieA=relation, movieC=relation... userA:rating, userB:rating...>
+			//collect the data for each movie, then do the multiplication
+		    Map<String, Double> relationMap =  new HashMap<>();
+		    Map<String, Double> ratingMap = new HashMap<>();
+
+		    for(Text value : values){
+		    	String str =  value.toString().trim();
+		    	if(str.contains("=")){
+		    		String[] m_r = str.split("=");
+		    		relationMap.put(m_r[0], Double.parseDouble(m_r[1]));
+		    	}else{
+		    		String[] u_r = str.split(":");
+		    		ratingMap.put(u_r[0],Double.parseDouble(u_r[1]));
+		    	}
+		    }
+
+		    for(String mv : relationMap.keySet()){
+		    	double relation =  relationMap.get(mv);
+
+		    	for(String user : ratingMap.keySet()){
+		    		double rating =  ratingMap.get(user);
+
+		    		context.write(new Text(user+":"+mv), new DoubleWritable(relation*rating));
+		    	}
+		    }
+
+
+
+		}
+	}
+
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+
+		Job job = Job.getInstance(conf);
+		job.setJarByClass(Multiplication.class);
+
+		ChainMapper.addMapper(job, CooccurrenceMapper.class, LongWritable.class, Text.class, Text.class, Text.class, conf);
+		ChainMapper.addMapper(job, RatingMapper.class, Text.class, Text.class, Text.class, Text.class, conf);
+
+		job.setMapperClass(CooccurrenceMapper.class);
+		job.setMapperClass(RatingMapper.class);
+
+		job.setReducerClass(MultiplicationReducer.class);
+
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(Text.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(DoubleWritable.class);
+
+		MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, CooccurrenceMapper.class);
+		MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, RatingMapper.class);
+
+		TextOutputFormat.setOutputPath(job, new Path(args[2]));
+		
+		job.waitForCompletion(true);
+	}
+}
